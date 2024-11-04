2a1cdc86a6067959d41c2ecc6106a3f454b707f8
elior estrin <eliore@ctera.com>
2024-11-05 00:41:48 +0200

    add sg

--- infra/vpc.tf
+++ infra/vpc.tf
@@ -12,3 +12,14 @@ module "vpc" {
     Environment = "dev"
   }
 }
+
+module "web_server_sg" {
+  source      = "terraform-aws-modules/security-group/aws//modules/http-80"
+
+  name        = "web-server"
+  description = "Security group for web-server with HTTP ports open within VPC"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+}
+
